eb0de9efcd88639b1f03d84700f55b5036652a5d
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-12-15 06:47:57 +0700

    feat: improve layout and add selected status label

--- views/odontogram/view.php
+++ views/odontogram/view.php
@@ -34,7 +34,7 @@ $this->registerJs("
 
     $(document).on('click', '.svg-container', function() {
         if (selectedStatusPath === null) {
-            alert('Select sebuah simbol odontogram!');
+            alert('Pilih sebuah simbol odontogram!');
         } else {
             var newToothPath = selectedStatusPath;
             var altValue = $(this).find('img').attr('alt');
@@ -46,7 +46,6 @@ $this->registerJs("
                 // Append the new image after the first img
                 if (firstImg.length) {
                     $('<img src=\"' + newToothPath + '\" id=\"' + idValue + '\" alt=\"' + altValue + '\" class=\"clickable-tooth-overlay\" style=\"display: inline; z-index=\"'+ selectedZIndex +'\"\">').insertAfter(firstImg);
-                    console.log('masuk if');
                 } else {
                     // If no img found, append at the end
                     $(this).append('<img src=\"' + newToothPath + '\" id=\"' + idValue + '\" alt=\"' + altValue + '\" class=\"clickable-tooth-overlay\" style=\"display: inline;\">');
@@ -58,12 +57,26 @@ $this->registerJs("
     $(document).on('click', '.hoverable-row', function() {
         selectedStatusPath = $(this).attr('status-path');
         selectedZIndex = $(this).attr('z-index');
+
+        var anchorElement = document.getElementById('selected-status-anchor');
+        anchorElement.innerText = $(this).attr('status-name');
     });
 ");
 ?>
 
 <div class="odontogram-view">
-    <div class="row">
+    
+<div class="row">
+        <div class="col-md-12">
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption font-red-sunglo">
+                        Odontogram
+                    </div>
+                </div>
+                <div class="portlet-body form">
+
+    <div class="row text-center">
         <?php
         for ($i = 18; $i >= 11; $i--) {
             // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
@@ -100,25 +113,237 @@ $this->registerJs("
             }
         }
         ?>
-        <br><br><br><br>
+        <?php
+        for ($i = 21; $i <= 28; $i++) {
+            // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
+            $matchingModel = Odontogram::findInDataProvider($i, $dataProvider);
 
-        <table class="table">
-    <thead>
-    </thead>
-    <tbody>
-        <?php $lowerLimit = 0; ?>
-        <?php $upperLimit = 2;  ?>
-        <?php foreach ($daftarStatusGigi as $index => $model): ?>
-            <?php if ($index >= $lowerLimit && $index <= $upperLimit): ?>
-                <tr class="hoverable-row" id=<?= 'status-' . $model->status_gigi_id ?> status-path=<?= Yii::getAlias('@web/'. $model->path ); ?>  z-index=<?= $model->z_index ?> >
-                    <td><?= $index + 1 ?></td>
-                    <td><?= $model->nama ?></td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+            if ($matchingModel !== null) {
+                $toothPath = Yii::getAlias('@web/svg/tooth' . $matchingModel->statusGigi->status_gigi_id . '.svg');
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($toothPath, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            } else {
+                $gigiModel = Gigi::findByNomor($i);
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($gigiModel->defaultStatusGigi->path, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            }
+        }
+        ?>
+        <br><br><br><br><br>
+    </div>
+    <div class="row text-center">
+    <?php
+        for ($i = 55; $i >= 51; $i--) {
+            // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
+            $matchingModel = Odontogram::findInDataProvider($i, $dataProvider);
 
+            if ($matchingModel !== null) {
+                $toothPath = Yii::getAlias('@web/svg/tooth' . $matchingModel->statusGigi->status_gigi_id . '.svg');
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($toothPath, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            } else {
+                $gigiModel = Gigi::findByNomor($i);
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($gigiModel->defaultStatusGigi->path, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            }
+        }
+        ?>
+        <?php
+        for ($i = 61; $i <= 65; $i++) {
+            // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
+            $matchingModel = Odontogram::findInDataProvider($i, $dataProvider);
+
+            if ($matchingModel !== null) {
+                $toothPath = Yii::getAlias('@web/svg/tooth' . $matchingModel->statusGigi->status_gigi_id . '.svg');
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($toothPath, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            } else {
+                $gigiModel = Gigi::findByNomor($i);
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($gigiModel->defaultStatusGigi->path, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            }
+        }
+        ?>
+        <br><br><br><br><br>
+    </div>
+    <div class="row text-center">
+    <?php
+        for ($i = 85; $i >= 81; $i--) {
+            // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
+            $matchingModel = Odontogram::findInDataProvider($i, $dataProvider);
+
+            if ($matchingModel !== null) {
+                $toothPath = Yii::getAlias('@web/svg/tooth' . $matchingModel->statusGigi->status_gigi_id . '.svg');
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($toothPath, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            } else {
+                $gigiModel = Gigi::findByNomor($i);
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($gigiModel->defaultStatusGigi->path, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            }
+        }
+        ?>
+        <?php
+        for ($i = 71; $i <= 75; $i++) {
+            // Cek jika ada model odontogram di dataProvider dengan gigi_id == $i
+            $matchingModel = Odontogram::findInDataProvider($i, $dataProvider);
+
+            if ($matchingModel !== null) {
+                $toothPath = Yii::getAlias('@web/svg/tooth' . $matchingModel->statusGigi->status_gigi_id . '.svg');
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($toothPath, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            } else {
+                $gigiModel = Gigi::findByNomor($i);
+                echo Html::tag(
+                    'div',
+                    Html::tag('div', $i, ['class' => 'tooth-label']) .
+                    Html::img($gigiModel->defaultStatusGigi->path, [
+                        'id' => "tooth-$i", 
+                        'alt' => "Tooth $i", 
+                        'class' => 'clickable-tooth',
+                        'status-gigi' => "1",
+                        'z-index' => "1"
+                    ]),
+                    ['class' => 'svg-container', 'style' => 'display: inline-block;']
+                );
+            }
+        }
+        ?>
+        <br><br><br><br><br>
+    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption font-red-sunglo">
+                        Simbol Odontogram
+                    </div>
+                </div>
+                <div class="portlet-body form">
+                    Selected:<br>
+                    <a href="#" class="btn btn-circle default" style="pointer-events: none;" id="selected-status-anchor">
+
+                    </a><br><br>
+                    <table class="table">
+                        <thead>
+                        </thead>
+                        <tbody>
+                            <?php $lowerLimit = 2; ?>
+                            <?php $upperLimit = 20;  ?>
+                            <?php foreach ($daftarStatusGigi as $index => $model): ?>
+                                <?php if ($index >= $lowerLimit && $index <= $upperLimit): ?>
+                                    <tr class="hoverable-row" id="<?= 'status-' . $model->status_gigi_id ?>" status-path="<?= Yii::getAlias('@web/'. $model->path ); ?>"  z-index=<?= $model->z_index ?> status-name="<?= $model->nama ?>">
+                                        <td><?= Html::img($model->path, [
+                                                'id' => "tooth-$i", 
+                                                'alt' => "Tooth $i", 
+                                                'class' => 'tooth-sample',
+                                            ]); ?>
+                                        </td>
+                                        <td><?= $model->nama ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
